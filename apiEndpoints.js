3ef674aaa94fdea89c4c90374f2880280574f1ec
Valerie Lui <vallq@users.noreply.github.com>
2020-02-05 17:17:39 +0800

    Initial Commit

---
+++ apiEndpoints.js
@@ -0,0 +1,12 @@
+const apiEndpoints = {
+  "0": "GET    /",
+  "1": "GET    /jumplings",
+  "2": "POST   /jumplings",
+  "3": "GET /jumplings/:id",
+  "4": "PUT /jumplings/:id",
+  "5": "DELETE /jumplings/:id",
+  "6": "-----------------------",
+  "7": "POST   /jumplings/presenters",
+  "8": "GET    /jumplings/presenters"
+};
+module.exports = apiEndpoints;
